e2ca21784d726a7d1f5e754bd0cd9c2127aa9bf5
timorwastaken <tirah4022@live.com>
2022-11-20 17:35:00 -0500

    added form along with data parser.

--- short.js
+++ short.js
@@ -6,35 +6,59 @@ const port = 3000;
 
 //setting view engine.
 app.set('view engine','ejs');
+//call bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 //function to get the value and output the key.
 function getURLKey(obj, value) {
   return Object.keys(URL).find(key => obj[key] === value);
 };
 
-//list of URLS
-const URL = 
-  {"https://www.google.ca/" : "x21jfl",
-  "https://ca.yahoo.com" : "va31km",
-  "https://www.youtube.com" :"y34sfa"
+//ID creator.
+function makeId(){
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 };
 
+//list of URLS
+const urlIndex = {
+  "x21jfl" : "https://www.google.ca",
+  "va31km" : "https://ca.yahoo.com",
+  "y34sfa" : "https://www.youtube.com",
+  "hello" : "https://www.twitter.com"
+};
+
+//homepage
 app.get('/', (req, res) => {
   res.send('First Node Server > thanks Gonzo');
 });
 
+
+//renders table as well as gets input for the url shortener.
 app.get('/home', function(req, res){
-  res.render('pages/home', {URL});
+  app.locals.urlIndex = urlIndex;
+  res.render('pages/home');
+  var newUrl = req.body.newUrl;
+  urlIndex[makeId()] = newUrl;
+  console.log(newUrl)
+
 });
 
-app.get('/short', (req, res) => {
-  res.render('/table');
+app.post('/home', function(req, res){
+ 
 });
+
 
 //if there is a shortened URL present it will redirect to the correct website, or else will bring them to a 404 page.
 app.get('/*', (req, res) => {
     var url = req.url;
-    var external = (getURLKey(URL, url.slice(1)));
+    var external = (getURLKey(urlIndex, url.slice(1)));
     if (external != undefined) {
       res.redirect(external);
     } else {
